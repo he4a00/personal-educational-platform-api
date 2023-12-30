d89df84e00449b9758a7279b02ecb4a3cec81d6c
he4a00 <https://github.com/he4a00>
2023-12-30 19:19:06 +0200

    يسش

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 app.use(cors({
   credentials: true,
-  origin: "https://personal-educational-platform.vercel.app/"
+  origin: "https://personal-educational-platform.vercel.app"
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
