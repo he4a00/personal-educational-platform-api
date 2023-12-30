ec7ec93ef3b82f2a324f64a191fb6acdbd476876
he4a00 <https://github.com/he4a00>
2023-12-30 19:14:51 +0200

    cors

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ connectDB();
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: "https://personal-educational-platform.vercel.app/"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
