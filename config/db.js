aec2f02b88d7911e7db503e9160f830e863a6f5f
he4a00 <https://github.com/he4a00>
2024-01-16 23:23:53 +0200

    sada

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`connected to db`);
   } catch (error) {
     console.log(error);
