52a60025044b0661a2b051f7e43749c85e0b9d6c
he4a00 <https://github.com/he4a00>
2024-01-16 23:48:16 +0200

    gfdgd

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`connected to db`);
+    // console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
     process.exit(1);
